5158dea6da76eef7e56bb0dfc43b4ad4d55d4fd7
Augustin MBARGA <augustin.mbarga@gmail.com>
2022-08-20 01:31:17 +0200

    Add Github logo & Header component done.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,11 @@
 // == IMPORTS ==
+import logo from '../../assets/images/logo-github.png';
 import './header.scss';
 
 export default function Header() {
     return(
         <div className="app-header">
-            <img src="" alt="Github logo" />
+            <img src={logo} alt="Github logo" />
         </div>
     );
 }
