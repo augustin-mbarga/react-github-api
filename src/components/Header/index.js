fb3785d2666ec82ef7e9382b9a503a2319dc7adb
Augustin MBARGA <augustin.mbarga@gmail.com>
2022-08-20 01:57:31 +0200

    Add styles folder.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 // == IMPORTS ==
-import logo from '../../assets/images/logo-github.png';
+import logo from "../../assets/images/logo-github.png";
 import './header.scss';
 
 export default function Header() {
