019dadb5c3913088cae9a5dedd5f9caa0c49aee4
Augustin MBARGA <augustin.mbarga@gmail.com>
2022-08-22 14:48:59 +0200

    Add link in HTML file to use semantic-ui framework.

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,9 +1,15 @@
 // == IMPORTS ==
+import { Form, Segment } from 'semantic-ui-react';
 import './searchBar.scss';
 
 export default function SearchBar() {
     return (
         <div className="searchbar">
+            <Segment>
+                <Form>
+                    <Form.Input fluid placeholder='First name' />
+                </Form>
+            </Segment>
             Searchbar
         </div>
     );
