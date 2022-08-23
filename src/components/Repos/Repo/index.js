8bbbb2372de3ed5939b72a6b52a235f6ee9ae70f
Augustin MBARGA <augustin.mbarga@gmail.com>
2022-08-23 03:05:58 +0200

    Add Cards done.

--- src/components/Repos/Repo/index.js
+++ src/components/Repos/Repo/index.js
@@ -1,10 +1,24 @@
 // == IMPORTS ==
+import { Card, Image } from 'semantic-ui-react';
 import './repo.scss';
 
 export default function Repos() {
     return (
-        <li className='repo'>
-            Repo
-        </li>
+        <Card className="repo">
+            <Image 
+                src='https://react.semantic-ui.com/images/avatar/large/matthew.png'
+            />
+            <Card.Content>
+                <Card.Header>Matthew</Card.Header>
+                <Card.Meta>
+                    <span className="date">
+                        Joined in 2015
+                    </span>
+                </Card.Meta>
+                <Card.Description>
+                    Matthew is a musician living in Nashville.
+                </Card.Description>
+            </Card.Content>
+        </Card>
     );
 }
