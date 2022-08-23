2e1424d2c4cf51c6856078f2ec8b15957afc4af0
Augustin MBARGA <augustin.mbarga@gmail.com>
2022-08-23 02:39:08 +0200

    Update Message component and remove Message.scss file content.

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -3,7 +3,7 @@ import { Message as MessageSUI } from 'semantic-ui-react';
 import './Message.scss';
 
 export default function Message() {
-    const message = `La recherche a donné 1232165 résultats.`;
+    const message = `La recherche a donné 1159901 résultats.`;
     return (
         <MessageSUI className="message">
             <p>{message}</p>
