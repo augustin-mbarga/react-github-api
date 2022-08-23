6a92ae8fe7b770806e7fc2d37aa9bc37f2b114f1
Augustin MBARGA <augustin.mbarga@gmail.com>
2022-08-23 13:37:56 +0200

    Add props in Repo component done.

--- src/components/Repos/Repo/index.js
+++ src/components/Repos/Repo/index.js
@@ -1,24 +1,33 @@
 // == IMPORTS ==
+import PropTypes from 'prop-types';
 import { Card, Image } from 'semantic-ui-react';
 import './repo.scss';
 
-export default function Repos() {
+export default function Repo({
+    imageUrl,
+    title,
+    owner,
+    description
+    }) {
     return (
         <Card className="repo">
-            <Image 
-                src='https://react.semantic-ui.com/images/avatar/large/matthew.png'
-            />
+            <Image src={imageUrl} wrapped ui={false} />
             <Card.Content>
-                <Card.Header>Matthew</Card.Header>
+                <Card.Header>{title}</Card.Header>
                 <Card.Meta>
-                    <span className="date">
-                        Joined in 2015
-                    </span>
+                    <span className="date">{owner}</span>
                 </Card.Meta>
                 <Card.Description>
-                    Matthew is a musician living in Nashville.
+                    {description}
                 </Card.Description>
             </Card.Content>
         </Card>
     );
+}
+// == PropTypes Validation ==
+Repo.propTypes = {
+    imageUrl: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    owner: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
 }
