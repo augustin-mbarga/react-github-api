4a51afaa07c1eaa81dca8e92c94cef42adbce87c
Augustin MBARGA <augustin.mbarga@gmail.com>
2022-08-23 12:59:02 +0200

    Add static props to message component done.

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,12 +1,15 @@
 // == IMPORTS ==
+import PropTypes from 'prop-types';
 import { Message as MessageSUI } from 'semantic-ui-react';
 import './Message.scss';
 
-export default function Message() {
-    const message = `La recherche a donné 1159901 résultats.`;
+export default function Message({content}) {
     return (
         <MessageSUI className="message">
-            <p>{message}</p>
+            <p>{content}</p>
         </MessageSUI>
     );
+}
+Message.propTypes = {
+    content: PropTypes.string.isRequired,
 }
