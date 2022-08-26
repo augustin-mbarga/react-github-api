7b53f04b342b8c86715dbdd20812d96c136d3ff8
Augustin MBARGA <augustin.mbarga@gmail.com>
2022-08-26 23:24:35 +0200

    Add useState hook in App component done.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,25 @@
+// == Import
+import { useState } from 'react';
+import { resultsDataFiltered as cleanRepos } from '../../selectors/data';
+
 import Header from '../Header';
 import SearchBar from '../SearchBar';
 import Message from '../Message';
 import Repos from '../Repos';
-import './App.scss';
+
 import resultsData from '../../data/repos';
-import { resultsDataFiltered as dataFiltered } from '../../selectors/data';
+import './App.scss';
 
 export default function App() {
+  const [count, setCount] = useState(resultsData.total_count);
+  const [results, setResults] = useState(resultsData.items);
+
   return (
     <div className="app">
       <Header />
       <SearchBar />
-      <Message counter={resultsData.total_count} />
-      <Repos results={dataFiltered(resultsData.items)} />
+      <Message counter={count} />
+      <Repos results={cleanRepos(results)} />
     </div>
   );
 }
