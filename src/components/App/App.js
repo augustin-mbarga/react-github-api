d4cc19853b280245ead528a5782bfe536d30c1e3
augustin-mbarga <augustin.mbarga@gmail.com>
2022-08-31 00:23:13 +0200

    Refacto of onChange function using SUI tools.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,20 +11,35 @@ import resultsData from "../../data/repos";
 import "./App.scss";
 
 export default function App() {
-  const [results, setResults] = useState(resultsData.items);
+  const [results, setResults] = useState(cleanRepos(resultsData.items));
   const [total, setTotal] = useState(resultsData.total_count);
   const [input, setInput] = useState("");
 
-  function handleChange(inputValue) {
-    setInput(inputValue);
+  // function handleChange(inputValue) {
+  //   setInput(inputValue);
+  // }
+
+  function handleSubmit(research) {
+    setInput(research);
+    const researchResults = cleanRepos(resultsData.items).filter(({ title }) =>
+      title.includes(input)
+    );
+    setResults(researchResults);
+    setTotal(researchResults.length);
+    // Reset
+    setInput("");
   }
 
   return (
     <div className="app">
       <Header />
-      <SearchBar inputValue={input} onChange={handleChange} />
+      <SearchBar
+        inputValue={input}
+        onChangeInputValue={(e, data) => setInput(data.value)}
+        onSubmit={handleSubmit}
+      />
       <Message counter={total} />
-      <Repos results={cleanRepos(results)} />
+      <Repos results={results} />
     </div>
   );
 }
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -4,20 +4,28 @@ import PropTypes from "prop-types";
 import { Form, Segment } from "semantic-ui-react";
 import "./searchBar.scss";
 
-export default function SearchBar({ inputValue, onChange }) {
-  const handleChange = (e) => {
-    onChange(e.target.value);
+export default function SearchBar({
+  inputValue,
+  onChangeInputValue,
+  onSubmit,
+}) {
+  //   const handleChange = (e) => {
+  //     onChange(e.target.value);
+  //   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(e.target.value);
   };
   return (
     <div className="searchbar">
       <Segment>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Input
             icon="search"
             iconPosition="left"
             placeholder="Saisissez votre recherche"
             value={inputValue}
-            onChange={handleChange}
+            onChange={onChangeInputValue}
           />
         </Form>
       </Segment>
@@ -27,5 +35,6 @@ export default function SearchBar({ inputValue, onChange }) {
 
 SearchBar.propTypes = {
   inputValue: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
+  onChangeInputValue: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
 };
