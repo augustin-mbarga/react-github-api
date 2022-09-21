a3a1e361c40d14a9d564e37c97ab1d152605b7cf
augustin-mbarga <augustin.mbarga@gmail.com>
2022-09-21 23:44:56 +0200

    Bugfix Menu component.

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,10 +2,31 @@ import { Menu as MenuSUI } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
 export default function Menu() {
+  const handleClick = (e) => {
+    const navBarLinks = document.querySelectorAll("a.item");
+    for (let i = 0; i < navBarLinks.length; i++) {
+      navBarLinks[i].innerText === e.target.text
+        ? navBarLinks[i].classList.add("active")
+        : navBarLinks[i].classList.remove("active");
+    }
+  };
   return (
     <MenuSUI>
-      <MenuSUI.Item name="search" content="Recherche" as={NavLink} to="/" />
-      <MenuSUI.Item name="faq" content="FAQ" as={NavLink} to="/faq" />
+      <MenuSUI.Item
+        name="search"
+        content="Recherche"
+        active
+        as={NavLink}
+        to="/"
+        onClick={handleClick}
+      />
+      <MenuSUI.Item
+        name="faq"
+        content="FAQ"
+        as={NavLink}
+        to="/faq"
+        onClick={handleClick}
+      />
     </MenuSUI>
   );
 }
